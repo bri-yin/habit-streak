116e940a0e4e4079b3aa98b38742e59ee8aadbfd
Brian <brianyin16@gmail.com>
2026-05-12 14:16:23 -0700

    setup

---
+++ habit-streak/habit-streak/habit-streak/habit_streakApp.swift
@@ -0,0 +1,17 @@
+//
+//  habit_streakApp.swift
+//  habit-streak
+//
+//  Created by Brian Yin on 5/12/26.
+//
+
+import SwiftUI
+
+@main
+struct habit_streakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
